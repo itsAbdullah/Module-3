cefd3e10510f52f16e95d590a1cb8451238753a0
itsAbdullah <khaneusafzaia@gmail.com>
2020-04-26 17:09:31 +0500

    new

---
+++ javaScript/app.js
@@ -0,0 +1,14 @@
+// prompt("What is Your name?");
+// prompt("What is him/her name?");
+// // prompt("Enter Your 2nd Bestfriend");
+
+// var lovescore = Math.random() * 100;
+// lovescore = Math.floor(lovescore) + 1;
+
+// // alert("your love Score is " + lovescore + " %");
+
+// if (lovescore > 80) {
+//   alert("Your lovescore is " + lovescore + "% Elaka da zalim Bacheya");
+// } else if (lovescore >= 70) {
+//   alert("Your lovescore is " + lovescore + "% Da dona meena");
+// } else alert("Your Lovescore is " + lovescore + "% Pasa Marah Ela dona");
---
+++ javaScript/app2.js
@@ -0,0 +1,58 @@
+// var guestName = prompt("What is Your Name?");
+
+// var guestList = ["Abdullah","Masoud","Muzamil", "Mansoor","Osama" ];
+// if(guestList.includes(guestName)){
+//   alert("Welcome!");
+// }else 
+//   alert("Sorry Sir Your name is Not Found At Guestlist");
+
+// alert(guestList);
+// console.log(guestList.includes("Osama"));
+
+
+
+
+
+// FizzBuzz PROGRAM
+
+
+
+
+
+// var output= [];
+// var Count = 1;
+
+// function fizzBuzz(){
+ 
+//   while( Count <= 100){
+// if(Count % 3 === 0 && Count % 5 === 0 ){
+//     // console.log(output["fizz"]);
+//     output.push("fizzBuzz");
+// }else if (Count % 3 === 0){
+//   // console.log(output["Buzz"]);
+//   output.push("fizz");
+// }else if (Count % 5 === 0){
+//   // console.log(output["fizzBuzz"]);
+//   output.push("Buzz");
+// }else{
+//   output.push(Count);
+// }
+//   Count++;
+// }
+//   console.log(output);
+
+// };
+// fizzBuzz();
+
+
+// var numberOfbottles = 99;
+
+// while (numberOfbottles >=0 ) {
+//   var bottleword = "Bottle";
+//   console.log()
+  
+// }
+
+
+
+
